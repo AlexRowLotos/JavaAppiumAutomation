3ccc246910641e14cdcc26e4f237295bf451a21c
Evgeny Dylevsky <eugene.dylevsky@gismart.com>
2018-10-05 02:39:00 +0300

    4nd_block_ex10_final*

--- src/SearchTests.java
+++ src/SearchTests.java
@@ -11,38 +11,38 @@ public class SearchTests extends CoreTestCase {
     protected void setUp() throws Exception {
         super.setUp();
     }
-//
-//    @Test
-//    public void testCancelSearch() {
-//        String search_word = "Java";
-//
-//        SearchPageObject searchPageObject = new SearchPageObject(driver);
-//
-//        searchPageObject.initSearchInput();
-//        searchPageObject.typeSearchText(search_word);
-//        searchPageObject.waitForSearchResult("Java");
-//        searchPageObject.clickCancelSearch();
-//
-//        assertTrue(searchPageObject.isSearchEmpty(search_word));
-//    }
-//
-//    @Test
-//    public void testCheckArticleTitle()  {
-//        String[] words = {
-//                "Java (programming language)",
-//                "Ruby (programming language)"
-//        };
-//
-//        SearchPageObject searchPageObject = new SearchPageObject(driver);
-//
-//        searchPageObject.initSearchInput();
-//        searchPageObject.typeSearchText(words[0]);
-//        searchPageObject.waitForSearchResult(words[0]);
-//        searchPageObject.clickByArticleWithSubstring(words[0]);
-//
-//        ArticlePageObject articlePageObject = new ArticlePageObject(driver);
-//        assertTrue(articlePageObject.assertTitleIsPresent());
-//    }
+
+    @Test
+    public void testCancelSearch() {
+        String search_word = "Java";
+
+        SearchPageObject searchPageObject = new SearchPageObject(driver);
+
+        searchPageObject.initSearchInput();
+        searchPageObject.typeSearchText(search_word);
+        searchPageObject.waitForSearchResult("Java");
+        searchPageObject.clickCancelSearch();
+
+        assertTrue(searchPageObject.isSearchEmpty(search_word));
+    }
+
+    @Test
+    public void testCheckArticleTitle()  {
+        String[] words = {
+                "Java (programming language)",
+                "Ruby (programming language)"
+        };
+
+        SearchPageObject searchPageObject = new SearchPageObject(driver);
+
+        searchPageObject.initSearchInput();
+        searchPageObject.typeSearchText(words[0]);
+        searchPageObject.waitForSearchResult(words[0]);
+        searchPageObject.clickByArticleWithSubstring(words[0]);
+
+        ArticlePageObject articlePageObject = new ArticlePageObject(driver);
+        assertTrue(articlePageObject.assertTitleIsPresent());
+    }
 
 
     @Test
